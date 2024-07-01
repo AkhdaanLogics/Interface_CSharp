2871f3c5cbfdfdbf56bc3cd78f7f969b6c3226fe
AkhdaanLogics <akhdaan@students.amikom.ac.id>
2024-07-01 21:03:06 +0700

    Add project files.

---
+++ pertemmuan_10/IPrinterWindows.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pertemmuan_10
+{
+    public interface IPrinterWindows
+    {
+        void Show();
+        void Print();
+    }
+}
---
+++ pertemmuan_10/LaserJet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pertemmuan_10
+{
+    public class LaserJet : IPrinterWindows
+    {
+        public void Print()
+        {
+            Console.WriteLine("LaserJet printer printing....");
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("LaserJet display dimension : 12*12");
+        }
+    }
+}
